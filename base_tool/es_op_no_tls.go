e81471bd8cd79d48056039176244f1f4932f1c22
haveTryTwo <liuzhongchao197@163.com>
2022-02-25 01:09:23 +0800

    1)Adding: close idle connection

--- base_tool/es_op_no_tls.go
+++ base_tool/es_op_no_tls.go
@@ -65,5 +65,7 @@ func httpReqInternal(op string, url string, body io.Reader, client *http.Client)
 		return nil, Error{Code: ErrIoUtilReadAllFailed, Message: err.Error()}
 	}
 
+    defer client.CloseIdleConnections()
+
 	return respByte, nil
 } // }}}
